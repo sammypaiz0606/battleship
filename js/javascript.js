9369ed32ab4a12d9edd2add5320a4c711c565404
Your Name <youremail@yourdomain.com>
2023-07-01 07:04:10 -0700

    styles/ colors

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,38 @@
 
+
+const gamesBoardContainer = document.querySelector('#gamesboard-container');
 const optionContainer = document.querySelector('.option-container');
 const flipButton = document.querySelector('#flip-button');
 
+// Option chioosing
+let angle = 0;
+
 function flip() {
     const optionShips = Array.from(optionContainer.children);
-    optionShips.forEach(optionShip => optionShip.style.transform = 'rotate(90deg)');
+
+    // if(angle === 0) {
+    //     angle = 90;
+    // } else {
+    //     angle = 0;
+    // }
+
+    // OR
+    angle = angle === 0 ? 90 : 0;
+
+    optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`);
 }
+
+// Creating Boards
+const width = 10;
+
+function createBoard(color) {
+    const gameBoardContainer = document.createElement('div');
+    gameBoardContainer.classList.add('game-board');
+    gameBoardContainer.style.backgroundColor = color;
+
+    gamesBoardContainer.append(gameBoardContainer);
+}
+createBoard('yellow');
+createBoard('pink');
 
 flipButton.addEventListener('click', flip);
