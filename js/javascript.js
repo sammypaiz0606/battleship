923cb26d981d66c2782f8c9d4323cbe3074d1f35
Your Name <youremail@yourdomain.com>
2023-07-01 06:09:55 -0700

    button to flip battleships

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+
+const optionContainer = document.querySelector('.option-container');
+const flipButton = document.querySelector('#flip-button');
+
+function flip() {
+    const optionShips = Array.from(optionContainer.children);
+    optionShips.forEach(optionShip => optionShip.style.transform = 'rotate(90deg)');
+}
+
+flipButton.addEventListener('click', flip);
