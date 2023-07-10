9334540ff55d33e08343987175c154d720f217e9
Your Name <youremail@yourdomain.com>
2023-07-10 12:33:53 -0700

    border/for loop for thebattlefield

--- js/javascript.js
+++ js/javascript.js
@@ -3,20 +3,22 @@
 const gamesBoardContainer = document.querySelector('#gamesboard-container');
 const optionContainer = document.querySelector('.option-container');
 const flipButton = document.querySelector('#flip-button');
+const sub = document.querySelector('.submarine');
 
-// Option chioosing
+
+// Option choosing
 let angle = 0;
 
 function flip() {
     const optionShips = Array.from(optionContainer.children);
-
+    
     // if(angle === 0) {
     //     angle = 90;
     // } else {
     //     angle = 0;
     // }
-
     // OR
+    // If angle is equal to 0 then return 90 otherwise return 0
     angle = angle === 0 ? 90 : 0;
 
     optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`);
@@ -25,14 +27,21 @@ function flip() {
 // Creating Boards
 const width = 10;
 
-function createBoard(color) {
+function createBoard(color, user) {
     const gameBoardContainer = document.createElement('div');
     gameBoardContainer.classList.add('game-board');
     gameBoardContainer.style.backgroundColor = color;
+    gameBoardContainer.id = user;
 
+    for(let i = 0; i < width * width; i++) {
+        const block = document.createElement('div');
+        block.classList.add('block');
+        block.id = i;
+        gameBoardContainer.append(block);
+    }
     gamesBoardContainer.append(gameBoardContainer);
 }
-createBoard('yellow');
-createBoard('pink');
+createBoard('yellow', 'player');
+createBoard('pink', 'computer');
 
 flipButton.addEventListener('click', flip);
